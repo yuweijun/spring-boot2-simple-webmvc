32b7cab7614895c34359945e3822dc1be7899d93
Weijun Yu <weijun.yu@sap.com>
2022-07-30 11:14:45 +0800

    simple webmvc v10

---
+++ v10/src/main/java/com/example/simple/spring/web/mvc/servlet/exception/HandlerExceptionResolverComposite.java
@@ -0,0 +1,46 @@
+package com.example.simple.spring.web.mvc.servlet.exception;
+
+import com.example.simple.spring.web.mvc.servlet.error.DefaultErrorAttributes;
+import org.springframework.core.Ordered;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Collections;
+import java.util.List;
+
+public class HandlerExceptionResolverComposite implements HandlerExceptionResolver, Ordered {
+
+    private List<HandlerExceptionResolver> resolvers;
+
+    private int order = Ordered.LOWEST_PRECEDENCE;
+
+    public void setOrder(int order) {
+        this.order = order;
+    }
+
+    public int getOrder() {
+        return this.order;
+    }
+
+    public void setExceptionResolvers(List<HandlerExceptionResolver> exceptionResolvers) {
+        this.resolvers = exceptionResolvers;
+    }
+
+    public List<HandlerExceptionResolver> getExceptionResolvers() {
+        return Collections.unmodifiableList(resolvers);
+    }
+
+    public boolean resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        DefaultErrorAttributes.storeErrorInRequest(request, ex);
+        if (resolvers != null) {
+            for (HandlerExceptionResolver handlerExceptionResolver : resolvers) {
+                final boolean resolved = handlerExceptionResolver.resolveException(request, response, handler, ex);
+                if (resolved) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+}
